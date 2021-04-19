a3fe653254f1d8d860a347551ae5da7546db25a1
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-19 09:25:35 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -47,8 +47,14 @@ export class RestaurantInput extends Component {
       </form>
     );
   }
+
+  mapDispatchToProps(dispatch) {
+    return {
+      addRestaurant: (newRest) => {
+        dispatch(addRestaurant(newRest))
+      }
+    }
+  }
 };
 
-
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput)
